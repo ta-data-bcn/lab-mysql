e4e074c0c97d183840f8dc9dd6dc11d1366a6a08
Adrià <adria.ferrer.abad@gmail.com>
2019-03-05 16:18:46 +0100

    Seeding file created
    @Annaviper

---
+++ your-code/seeding.sql
@@ -0,0 +1,8 @@
+INSERT INTO cars (record_id, vin, manufacturer, model, year, color)
+	VALUES (0,'3K096I98581DHSNUP','Volkswagen','Tiguan',2019,'Blue'),(1,'ZM8G7BEUQZ97IH46V','Peugeot','Rifter',2019,'Red'), (2,'RKXVNNIHLVVZOUB4M','Ford','Fusion',2018,'White'),(3,'HKNDGS7CU31E9Z7JW','Toyota','RAV4',2018,'Silver'),(4,'DAM41UDN3CHU2WVF6','Volvo','V60',2019,'Gray'),(5,'DAM41UDN3CHU2WVF6','Volvo','V60 Cross Country',2019,'Gray');
+INSERT INTO customers (record_id, customer_id, name, phone_number, email, address, city, state_province, country, zip_postal)
+	VALUES (0,10001,'Pablo Picasso','+34 636 17 63 82','','Paseo de la Chopera, 14','Madrid','Madrid','Spain',28045),(1,20001,'Abraham Lincoln','+1 305 907 7086','','120 SW 8th St','Miami','Florida','United States',33130),(2,30001,'Napoléon Bonaparte','+33 1 79 75 40 00','','40 Rue du Colisée','Paris','Île-de-France','France',75008);
+INSERT INTO salesperson (record_id, staff_id, name, store)
+	VALUES(0,000001,'Peter Cruiser','Madrid'),(1,00002,'Anna Sthesia','Barcelona'),(2,00003,'Paul Molive','Berlin'),(3,00004,'Gail Forcewind','Paris'),(4,00005,'Paige Turner','Mimia'),(5,00006,'Bob Frapples','Mexico City'),(6,00007,'Walter Melon','Amsterdam'),(7,00008,'Shonda Leer','São Paulo');
+INSERT INTO invoices (record_id, invoice_number,date_sale, car, customer, salesperson)
+	VALUES (0,852399038,'2018-08-22',0,1,3), (1,731166526,'2018-12-31',3,0,5),(2,271135104,'2019-01-22',2,2,7)
