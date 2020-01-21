9a609916338dfd6bb5a6f64fe049b70641b40b78
Oriol Cunill <cunillati@gmail.com>
2020-01-21 18:22:34 +0100

    update customer emails

---
+++ your-code/update.sql
@@ -0,0 +1,3 @@
+UPDATE Customer SET email = 'ppicasso@gmail.com' WHERE name = 'Pablo Picasso';
+UPDATE Customer SET email = 'lincoln@us.gov' WHERE name = 'Abraham Lincoln';
+UPDATE Customer SET email = 'hello@napoleon.me' WHERE name = 'Napoléon Bonaparte';
