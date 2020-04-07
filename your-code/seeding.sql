0f8c92f8ef2b32719815cf9d2520531b6d7361e1
Aitor Quinza <aitor@quinza.es>
2020-04-07 12:50:04 +0200

    Fixed Seeding.sql Customers ID

--- your-code/seeding.sql
+++ your-code/seeding.sql
@@ -8,9 +8,9 @@ VALUES (0, '3K096I98581DHSNUP', 'Volkswagen', 'Tiguan', '2019', 'Blue'),
        (5, 'DAM41UDN3CHU2WVF6', 'Volvo', 'V60 Cross Country', '2019', 'Gray');
 
 INSERT INTO `Customers` (`ID`, `Customer ID`, `Name`, `Phone`, `Email`, `Address`, `City`, `State`, `Country`, `Postal`)
-VALUES (1, 10001, 'Pablo Picasso', '+34636176382', NULL, 'Paseo de la Chopera, 14', 'Madrid', 'Madrid', 'Spain', 28045),
-       (2, 20001, 'Abraham Lincoln', '+13059077086', NULL, '123 SW 8th St', 'Miami', 'Florida', 'United States', 33130),
-       (3, 30001, 'Napoleón Bonaparte', '+33179754000', NULL, '40 Rue du Colisée', 'Paris', 'Île-de-France', 'France',
+VALUES (0, 10001, 'Pablo Picasso', '+34636176382', NULL, 'Paseo de la Chopera, 14', 'Madrid', 'Madrid', 'Spain', 28045),
+       (1, 20001, 'Abraham Lincoln', '+13059077086', NULL, '123 SW 8th St', 'Miami', 'Florida', 'United States', 33130),
+       (2, 30001, 'Napoleón Bonaparte', '+33179754000', NULL, '40 Rue du Colisée', 'Paris', 'Île-de-France', 'France',
         75008);
 
 INSERT INTO `Invoices` (`ID`, `Invoice Number`, `Date`, `Car`, `Customer`, `Salesperson`)
